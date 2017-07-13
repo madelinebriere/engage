f52f733e2732c5ad1852dc21582e79fb5da177e6
Katherine Guo <kguo.zteam@zuora.com>
2017-07-13 13:30:54 -0700

    added base.html

--- webapp/views.py
+++ webapp/views.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import Context, loader
+
+import os
 
 
 def charitiesList(request):
-    return HttpResponse("Hello, world. You're at charities.")
-
+    return render_to_response('webapp/base.html')
 
 def index(request):
     return HttpResponse("Hi!!!")
