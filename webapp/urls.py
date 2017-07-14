62127506c5244e015d2f2da9a8d5354fe8e03816
Madeline Briere <mbriere.zteam@zuora.com>
2017-07-14 09:45:02 -0700

    Home page

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'charities', views.charitiesList, name='charitiesList'),
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
     url(r'dashboard', views.dashboard, name='dashboard'),
 ]
