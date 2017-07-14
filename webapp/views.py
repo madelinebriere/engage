2b8992f6ee3952f9611e04e3219d81b4c2f17d65
Katherine Guo <kguo.zteam@zuora.com>
2017-07-13 22:53:45 -0700

    voting, sorting, and adding new charity works

--- webapp/views.py
+++ webapp/views.py
@@ -3,24 +3,35 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, render_to_response
 
+from webapp.models import Charity
+from pprint import pprint
 # Create your views here.
 
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.template import RequestContext
 
 def dashboard(request):
     return render_to_response("webapp/dashboard.html")
 
 def charitiesList(request):
-	charities = ['charity water', 'breast cancer awareness', 'wah'];
-	return render_to_response('webapp/charitieslist.html', {'charities':charities})
+	if request.method == 'POST': 
+		name = request.POST.get("charity_name")
+		description = request.POST.get("charity_description")
+		charity_object = Charity(name = name, description = description, votes = 0)
+		charity_object.save()
+	return render(request, 'webapp/charitieslist.html', {'charities':Charity.objects.all()})
 
 def index(request):
 	return HttpResponse("Hello, world. You're at charities.")
 
 def new_charity_form(request):
+	return render(request, 'webapp/new_charities_form.html')
+
+def charity_vote(request):
 	if request.method == 'POST': 
-		form = NewCharityForm(request.POST)
-		if form.is_valid():
-			return HttpResponseRedirect('/home/charities')
-	return render_to_response('webapp/new_charities_form.html')
+		id = request.POST.get("charity_id")
+		charity_object = Charity.objects.get(pk=id)
+		charity_object.votes += 1
+		charity_object.save()
+	return render(request, 'webapp/charitieslist.html', {'charities':Charity.objects.all()})
